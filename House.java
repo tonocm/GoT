c03f5a151b91ae1bf24e87b3d81c2c1e04c64a37
tonocm <tonocm94@hotmail.com>
2015-03-28 20:58:23 -0400

    Updated House Object and Card

--- House.java
+++ House.java
@@ -1,10 +1,15 @@
 public class House{
   
   String name;
-  ArrayList<
+  ArrayList<Land> lands;
+  ArrayList<Sea> seas;
+  ArrayList<Card> handCards;
   
-  public House(String name,){
-    
+  public House(String name, ArrayList<Land> lands, ArrayList<Sea> seas, ArrayList<Card> handCards){
+    this.name = name;
+    this.lands = lands;
+    this.seas = seas;
+    this.handCards = handCards;
   }
   
 }
