ace64a131249d106f3be6d1672f683159ec99925
tonocm <tonocm94@hotmail.com>
2015-03-31 23:12:20 -0400

    Antonio's Branch

--- GoT.java
+++ GoT.java
@@ -1,11 +1,47 @@
+import java.io.*;
+import java.util.*;
+
 class GoT{
-  public static void main (String[] args){
+  public static void main (String[] args) throws IOException{
     
-    Sea test = new Sea("pacific");
-    
-    System.out.println(test.name);
-    System.out.println(test.available);
-    System.out.println(test.occupiedBy);
+    Board board = makeBoard();
     
   }
+  
+  public static Board makeBoard() throws IOException{
+    int i;
+    File board = new File("board.txt");
+    Scanner file = new Scanner(board).useDelimiter("\t");
+    
+    int type, castles, supplies, crowns;
+    String name, adjacent;
+    
+    while(file.hasNext()){
+      
+      /*
+       * 1 - [Sea = 1, Land = 0]
+       * 2 - Name
+       * 3 - [Castle = 0, Stronghold = 1]
+       * 4 - Supplies
+       * 5 - Crowns
+       * 6 - Adjacent Territories
+       */
+        /* 1 */ type = file.next();
+        /* 2 */ name = file.next();
+        /* 3 */ castles = file.next();
+        /* 4 */ supplies = file.next();
+        /* 5 */ crowns = file.next();
+        /* 6 */ adjacent = file.next();
+      
+      if(type == 0){
+          
+        }
+        else{
+          
+        }
+      
+    }
+    return new Board();
+  }
+  
 }
